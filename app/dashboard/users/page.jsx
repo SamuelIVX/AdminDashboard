c00c0b7aa5cd495bc281560b87c38f1c9165da39
Samuel Hernandez Balderas <samuel05.hb@gmail.com>
2025-01-06 23:05:28 -0500

    Fetching Items with Pagination using URL queries

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -7,7 +7,9 @@ import Pagination from "@/app/ui/dashboard/pagination/pagination";
 
 const UsersPage = async ({ searchParams }) => {
   const q = searchParams?.q || "";
-  const users = await fetchUsers(q);
+  const page = searchParams?.page || 1;
+  const { count, users } = await fetchUsers(q, page);
+
   return (
     <div className={styles.container}>
       <div className={styles.top}>
@@ -65,7 +67,7 @@ const UsersPage = async ({ searchParams }) => {
           })}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count} />
     </div>
   );
 };
